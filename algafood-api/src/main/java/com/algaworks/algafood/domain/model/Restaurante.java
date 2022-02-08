30a68163d4ef121eac69bfbbc3682605923ae771
Thiago Henrique <thiago.henrique.25@hotmail.com>
2022-02-08 14:16:55 -0300

    @Embedded

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -44,6 +45,9 @@ public class Restaurante implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@Embedded
+	private Endereco endereco;
 
 	@JsonIgnore
 	@ManyToMany
