14d26c98ad00b53b099d576852d33d70b4651768
Thiago Henrique <thiago.henrique.25@hotmail.com>
2022-02-13 20:29:52 -0300

    Revisando e ajustando as mensagens de validação com o uso de DTOs

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/restaurante/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/restaurante/RestauranteController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
