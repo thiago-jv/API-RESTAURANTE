43b06fccae32b506a79e68c9691e6c48ee64a5eb
thiago <dbc.melo@unimedpoa.com.br>
2022-02-21 13:51:41 -0300

    Implementando upload de arquivo com multipart/form-data

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/restaurante/produto/RestauranteProdutoFotoController.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.api.controller.restaurante.produto;
+
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+
+@RestController
+@RequestMapping("/restaurante/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+	@PutMapping
+	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput) {
+		
+		var nomeArquivo = UUID.randomUUID().toString() + "_" +fotoProdutoInput.getArquivo().getOriginalFilename();
+		
+		// informando onde quero salvar o arquivo
+		var arquivoFoto = Path.of("C:\\Users\\thiago.melo\\Documents\\arquivos\\catalogo", nomeArquivo);
+		
+		System.out.println(fotoProdutoInput.getDescricao());
+		System.out.println(arquivoFoto);
+		System.out.println(fotoProdutoInput.getArquivo().getContentType());
+		
+		try {
+			// transferindo o objeto para o caminho especificado
+			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
