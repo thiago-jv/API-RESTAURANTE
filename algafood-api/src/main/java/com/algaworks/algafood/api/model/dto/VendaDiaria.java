3857e298eed271d440b356110a9f2c957664c8d4
thiago <dbc.melo@unimedpoa.com.br>
2022-02-21 08:03:23 -0300

    BUG ->  13.16. Tratando time offset na agregação de vendas diárias por data - com bug no momento, corrigir data posteriormente

--- algafood-api/src/main/java/com/algaworks/algafood/api/model/dto/VendaDiaria.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/dto/VendaDiaria.java
@@ -2,19 +2,32 @@ package com.algaworks.algafood.api.model.dto;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class VendaDiaria {
 
-	//trabalhar a solução depois
-	//private String data;
-	
+	// trabalhar a solução depois
+	private String data;
+
 	private Long totalVendas;
-	
+
 	private BigDecimal totalFaturado;
+
+	public VendaDiaria() {
+	}
+
+	public VendaDiaria(Long totalVendas, BigDecimal totalFaturado) {
+		this.totalVendas = totalVendas;
+		this.totalFaturado = totalFaturado;
+	}
+	
+	public VendaDiaria(String data, Long totalVendas, BigDecimal totalFaturado) {
+		this.data = data;
+		this.totalVendas = totalVendas;
+		this.totalFaturado = totalFaturado;
+	}
+
 }
