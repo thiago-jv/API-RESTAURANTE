46f5c32db26d1fc1203e97c3f2b87a02fe3a8f34
thiago <dbc.melo@unimedpoa.com.br>
2022-03-08 16:10:04 -0300

    Gerando a documentação da API em HTML com Swagger UI e SpringFox

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+public class SpringFoxConfig implements WebMvcConfigurer {
 
 	@Bean
 	public Docket docket() {
@@ -19,4 +21,13 @@ public class SpringFoxConfig {
 				.apis(RequestHandlerSelectors.any())
 						.build();
 	}
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+			.addResourceLocations("classpath:/META-INF/resources/");
+		
+		registry.addResourceHandler("/webjars/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
 }
