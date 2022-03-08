056d299157bb27d196a4a66e7c268d27f785c4f2
thiago <dbc.melo@unimedpoa.com.br>
2022-03-08 16:20:04 -0300

    Descrevendo informações da API na documentação

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,7 +23,18 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
-						.build();
+				.paths(PathSelectors.any())
+						.build()
+						.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("AlgaFood API")
+				.description("Api para controle de restaurantes")
+				.version("1")
+				.contact(new Contact("Thiago Henrique", "https://github.com/thiago-jv/", "thiago.henrique.25@hotmail.com"))
+				.build();
 	}
 	
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
