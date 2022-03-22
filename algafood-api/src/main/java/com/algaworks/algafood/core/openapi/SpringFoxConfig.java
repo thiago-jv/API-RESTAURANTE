24a51261f381a0fa2f288ee094ec117817cb3147
thiago <dbc.melo@unimedpoa.com.br>
2022-03-22 11:59:06 -0300

    Descrevendo parâmetros globais em operações onde é informações os parametros no swagger

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -22,6 +22,7 @@ import com.fasterxml.classmate.TypeResolver;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
@@ -55,6 +56,14 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 		  				  .globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
 		  				  .globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
 		  				  .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
+		  				  .globalOperationParameters(Arrays.asList(
+		  						  new ParameterBuilder()
+		  						  .name("campos")
+		  						  .description("Nomes das propreidades para filtar nas resposta separados por vírgula")
+		  						  .parameterType("query")
+		  						  .modelRef(new ModelRef("string"))
+		  						  .build();
+		  						  ))
 		  				  .ignoredParameterTypes(ServletWebRequest.class)
 		  				  .directModelSubstitute(Pageable.class, PageableOpenApi.class)
 		  				  .alternateTypeRules(AlternateTypeRules.newRule(
