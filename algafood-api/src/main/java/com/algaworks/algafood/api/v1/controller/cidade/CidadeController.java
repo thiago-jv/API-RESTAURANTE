59e5dd70b76117e2743c9322f6d86a9cb8e8d250
thiago <dbc.melo@unimedpoa.com.br>
2022-04-13 15:20:02 -0300

    Implementando o versionamento da API por URI

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/controller/cidade/CidadeController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/controller/cidade/CidadeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,6 @@ import com.algaworks.algafood.api.v1.assembler.cidade.CidadeModelAssembler;
 import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 import com.algaworks.algafood.api.v1.openapi.controller.CidadeControllerOpenApi;
-import com.algaworks.algafood.core.web.AlgaMediaTypes;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.exception.estado.EstadoNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
@@ -28,7 +28,7 @@ import com.algaworks.algafood.domain.repository.cidade.CidadeRepository;
 import com.algaworks.algafood.domain.service.cidade.CadastroCidadeService;
 
 @RestController
-@RequestMapping(path = "/cidades", produces = AlgaMediaTypes.V1_APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v1/cidades", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CidadeController implements Serializable, CidadeControllerOpenApi {
 
 	/**
--- algafood-api/src/main/java/com/algaworks/algafood/api/v2/controller/cidade/CidadeControllerV2.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v2/controller/cidade/CidadeControllerV2.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,6 @@ import com.algaworks.algafood.api.v2.assembler.cidade.CidadeInputDisassemblerV2;
 import com.algaworks.algafood.api.v2.assembler.cidade.CidadeModelAssemblerV2;
 import com.algaworks.algafood.api.v2.model.CidadeModelV2;
 import com.algaworks.algafood.api.v2.model.input.CidadeInputV2;
-import com.algaworks.algafood.core.web.AlgaMediaTypes;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.exception.estado.EstadoNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
@@ -27,7 +27,7 @@ import com.algaworks.algafood.domain.repository.cidade.CidadeRepository;
 import com.algaworks.algafood.domain.service.cidade.CadastroCidadeService;
 
 @RestController
-@RequestMapping(path = "/cidades", produces = AlgaMediaTypes.V2_APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v2/cidades", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CidadeControllerV2 implements Serializable {
 
 	/**
