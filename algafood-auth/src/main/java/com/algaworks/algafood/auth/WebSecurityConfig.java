5e21676a41d7d7d04fb481235e491c3dc8bfaa57
thiago <dbc.melo@unimedpoa.com.br>
2022-05-13 17:07:47 -0300

    Configurando o fluxo Authorization Server com Password Credentials e Opaque Tokens

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
@@ -2,11 +2,10 @@ package com.algaworks.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,25 +21,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	      .roles("ADMIN");
 	}
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	  http.httpBasic()
-	   .and()
-	    .authorizeRequests()
-	     .antMatchers("/v1/cidades/**").permitAll()
-	     .anyRequest().authenticated()
-	     
-	     .and()
-	      .sessionManagement()
-	       .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	       
-	       .and()
-	          .csrf().disable();
-	}
-	
 	// infomando que vamos usar uma cripitografia
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+	  return super.authenticationManager();
 	}
 }
