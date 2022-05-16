6a62910afedc8a62cc570c40eff5c5d90413e4c8
thiago <dbc.melo@unimedpoa.com.br>
2022-05-16 11:47:44 -0300

    Configurando o Refresh Token Grant Type no Authorization Server

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,6 +21,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 	 clients.inMemory()
@@ -31,7 +35,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	  .and()
 	   .withClient("checktoken")
 	    .secret(passwordEncoder.encode("check123"))
-	    .authorizedGrantTypes("password")
+	    .authorizedGrantTypes("password", "refresh_token")
 		  .scopes("write", "read")
 		  .accessTokenValiditySeconds(60 * 60 * 6);
 		
@@ -45,7 +49,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-	   endpoints.authenticationManager(authenticationManager);
+	   endpoints
+	    .authenticationManager(authenticationManager)
+	    .userDetailsService(userDetailsService);
+	   
 	}
 	
 	
